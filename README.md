e0a67c0deaf6add8eb6a6197973109eac39b9481
Westporch <westporch@gmail.com>
2017-07-23 08:10:36 +0900

    Day4 수업을 위한 기본 환경 셋팅 (day3의 마지막 환경)

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# askdjango-day04-review
-AskDjango Day4 오프라인 강의 복습
+# AskDjango Day4 오프라인 강의 복습
+
+## 강의
+
+날짜: 2017.07.16(일) 13:00 ~ 17:00
+
+장소: [모임플러스](http://map.naver.com/?mapmode=0&lng=f27e089e43904d8797309ac76534197c&pinId=32514029&lat=25d147cf133d88881094ba67f6383ba8&dlevel=11&enc=b64&pinType=site)
+
+## 수업 내용
+
+### 장고 모델과 마이그레이션
+
+더 이상 습관적으로 makemigrations/migrate 명령을 입력하지 마세요. 데이터는 방어적/보수적으로 관리해야합니다. 마이그레이션에 대해서 정확히 이해하고, 우리의 소중한 데이터를 안>전하게 관리해봅시다.
+
+### 장고 모델을 통한 추가/수정/삭제/조회 구현
+
+장고의 핵심 기능인 모델을 잘 활용하신다면, 많게는 60~70% 이상의 코드를 절감하실 수 있습니다. 모델을 잘 이해하고, 데이터를 효율적으로 관리해봅시다.
+
+## 테스트 환경
+
+| 항목 | 내용 |
+| :--: | :--: |
+| Django | v1.11.3 |
+| Python | v3.5 |
+| Django-debug-toolbar | v1.8 |
+
+### 사용 방법
+
+```
+# git clone https://github.com/westporch/askdjango-day04-review.git
+# cd YOUR_PATH/askdjango-day04-review/myenv/bin
+# source activate
+(myenv) # pip3 install django==1.11.3
+(myenv) # pip3 install django-debug-toolbar==1.8
+```
---
+++ dev/askdjango/blog/views.py
@@ -0,0 +1,82 @@
+import os
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+''' from django.shortcuts import render에 대한 설명
+
+render를 import하지 않고 3~4줄로 작성하는 것은 오래된 코드 방식이다.
+간단히 from django.shortcuts import render 1줄로 작성한다.
+'''
+
+def post_list(request):
+	#print(request.META['REMOTE_ADDR']) # 웹으로 django 접속시 remote host의 ip를 runserver 콘솔에 출력한다.
+	return render(request, 'blog/post_list.html')
+
+def post_list1(request):
+	'FBV: 직접 문자열로 HTML형식 응답하기'
+
+	name = '공유'
+	return HttpResponse('''
+		<h1>AskDjango</h1>
+		<p> {name} </p>
+		<p>여러분의 파이썬&장고 페이스메이커가 되겠습니다.</p>'''.format(name=name))
+
+def post_list2(request):
+	'FBV: 템플릿을 통해 HTML형식 응답하기'
+
+	print(request.META['REMOTE_ADDR']) # 웹으로 django 접속시 remote host의 ip를 runserver 콘솔에 출력한다.
+
+	name = '공유'
+	response = render(request, 'blog/post_list.html', {'name': name})	# 좌 측의 name을 우측의 name으로 넘긴다.
+	return response
+
+def post_list3(request):
+	'FBV: JSON 형식 응답하기'
+
+	return JsonResponse({
+		'message': '안녕, 파이썬&장고',
+		'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+	}, json_dumps_params={'ensure_ascii': False})
+
+def excel_download(request):
+	'FBV: 엑셀 다운로드 응답하기'
+	
+	filepath = '/home/westporch/test.xlsx'
+	filename = os.path.basename(filepath)
+
+	with open(filepath, 'rb') as f:
+		response = HttpResponse(f, content_type='application/vnd.ms-excel')
+		# 필요한 응답헤더 세팅
+		response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+		return response
+
+#def mysum(request, x):
+#	''' 인자: 정수 1개
+#    리턴값: URL에 입력한 값(정수 1개)을 리턴하여 웹페이지에 출력한다.
+#	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (999가 출력된다.)
+#				http://192.168.0.17:8080/blog/sum/999/
+#	'''
+#	return HttpResponse(int(x))
+
+#def mysum(request, x, y):
+#	''' 인자: 정수 2개
+#    리턴값: URL에 입력한 값(정수 2개)을 더한 뒤 리턴하여 웹페이지에 출력한다.
+#	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (1000이 출력된다.)
+#				http://192.168.0.17:8080/blog/sum/999/1
+#	'''
+#	return HttpResponse(int(x) + int(y))
+
+def mysum(request, x, y=0, z=0):
+	''' 인자: 정수 3개
+    리턴값: URL에 입력한 값(정수 3개)을 더한 뒤 리턴하여 웹페이지에 출력한다.
+	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (1004가 출력된다.)
+				http://192.168.0.17:8080/blog/sum/999/1/4
+	'''
+	return HttpResponse(int(x) + int(y) + int(z))
+
+def greet_korean(request, korean_name, age):
+	'''인자: 2개 (한글 이름, 나이)
+	리턴값(예): 안녕하세요. 공유. 37살이시네요.
+    사용법(예): http://192.168.0.17:8080/blog/hello/%EA%B3%B5%EC%9C%A0/37/
+	'''
+	return HttpResponse("안녕하세요. {korean_name}. {age}살이시네요.".format(korean_name=korean_name, age=int(age)))
