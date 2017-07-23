47bfa0157e2559e9428b9cf175bce89835dc2cb0
Westporch <westporch@gmail.com>
2017-07-23 10:28:26 +0900

    blog.views.post_list 뷰에서 Post 목록 구현

--- dev/askdjango/blog/views.py
+++ dev/askdjango/blog/views.py
@@ -1,4 +1,5 @@
 import os
+from .models import Post
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
@@ -10,7 +11,8 @@ render를 import하지 않고 3~4줄로 작성하는 것은 오래된 코드 방
 
 def post_list(request):
 	#print(request.META['REMOTE_ADDR']) # 웹으로 django 접속시 remote host의 ip를 runserver 콘솔에 출력한다.
-	return render(request, 'blog/post_list.html')
+	qs = Post.objects.all()	# 아직 DB에서 데이터를 가져오지 않았음
+	return render(request, 'blog/post_list.html', {'post_list': qs,}) # post_list를 qs에 저장함
 
 def post_list1(request):
 	'FBV: 직접 문자열로 HTML형식 응답하기'
